e018cd449bad713109667a2dc02853bf27f27301
tisumaki <84583619+tisumaki@users.noreply.github.com>
2022-02-15 22:04:57 +0200

    Second draft

--- resources/script.js
+++ resources/script.js
@@ -5,8 +5,9 @@ var navbar = document.querySelector('.nav-container');
 var sticky = navbar.offsetTop;
 
 function stickyNav() {
-  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-    navbar.classList.add("sticky")
+  if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
+    navbar.classList.add("sticky");
+    
   } else {
     navbar.classList.remove("sticky");
   }
