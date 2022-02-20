7903b6f36bbe8b4f7b7a4786c2dd811055ef1076
tisumaki <84583619+tisumaki@users.noreply.github.com>
2022-02-21 00:29:16 +0200

    Added form section (incomplete php)

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "makelatiia@hotmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $company = $_POST['company'];
+    $subject = "Form submission";
+    $message = $name . " from" . $company . " wrote the following:" . "\n\n" . $_POST['message'];
+
+    $headers = 'From: webmaster@example.com'       . "\r\n" .
+                 'Reply-To: webmaster@example.com' . "\r\n" .
+                 'X-Mailer: PHP/' . phpversion();
+    mail($to, $subject, $message, $headers);
+    
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
