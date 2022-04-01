074199a3ee553ee21706f7e7963298aee29ac5ad
tisumaki <84583619+tisumaki@users.noreply.github.com>
2022-04-01 14:27:20 +0300

    Added functionality to the read more/less buttons

--- resources/script.js
+++ resources/script.js
@@ -11,12 +11,31 @@ $("document").ready(function($){
 });
 
 $(document).ready(function() {
-    $(".read").click(function() {
-        $(this).siblings(".more").toggle();
+    let section1 = $(".more-services").closest("section");
+    $(".top").click(function() {
+        $(this).siblings(".more-services").toggle();
         if($(this).text()=='Read more'){
             $(this).text('Read less');
         } else {
             $(this).text('Read more');
+            $('html,body').animate({
+                scrollTop: $(section1).offset().top},
+                'fast');
+        }
+    })
+});
+
+$(document).ready(function() {
+    let section2 = $(".more-about").closest("section");
+    $(".read").click(function() {
+        $(this).siblings(".more-about").toggle();
+        if($(this).text()=='Read more'){
+            $(this).text('Read less');
+        } else {
+            $(this).text('Read more');
+            $('html,body').animate({
+                scrollTop: $(section2).offset().top},
+                'fast');
         }
     })
 });
